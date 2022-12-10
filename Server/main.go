de361c7422367d9c44d75f05d4b244b6e7ec9047
leartpro <lenni.protte@gmx.de>
2022-12-10 17:12:35 +0100

    Init Go

---
+++ Server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+func handleWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Websocket endpoint hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+V\n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func main() {
+	fmt.Println("Chat App v0.01")
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		handleWs(pool, w, r)
+	})
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+}
---
+++ Server/web/Pool.go
@@ -0,0 +1,49 @@
+package web
+
+import "fmt"
+
+type Pool struct {
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan Message
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+}
+
+func (this *Pool) Start() {
+	for {
+		select {
+		case client := <-this.Register:
+			this.Clients[client] = true
+			fmt.Println("Size of Connection pool", len(this.Clients))
+			for client, _ := range this.Clients {
+				fmt.Println(client)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
+			}
+			break
+		case client := <-this.Unregister:
+			delete(this.Clients, client)
+			fmt.Println("Size of Connection pool", len(this.Clients))
+			for client, _ := range this.Clients {
+				client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
+			}
+			break
+		case message := <-this.Broadcast:
+			fmt.Println("Sending messageInput to all clients in pool")
+			for client, _ := range this.Clients {
+				if err := client.Conn.WriteJSON(message); err != nil {
+					fmt.Println(err)
+					return
+				}
+			}
+		}
+	}
+}
