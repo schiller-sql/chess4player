398745048834e662dd33033d480a9aa3f368f522
Tony <65500763+bit-burger@users.noreply.github.com>
2023-01-01 20:18:17 +0100

    flutter: enter web-socket url via .env file

--- client/flutter/lib/config.dart
+++ client/flutter/lib/config.dart
@@ -1,7 +1,15 @@
 import 'package:chess_4p_connection/chess_4p_connection.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<ChessConnection> getConnection() async {
-  // TODO: change to .env file
-  final wsUri = Uri.parse("ws://localhost:8080");
+  await dotenv.load(fileName: "../.env");
+  final url = dotenv.maybeGet("URL");
+  assert(
+    url != null,
+    "create a '.env' file in the 'client' folder "
+    "with a web-socket URL as follows: "
+    "URL='[here comes the url]'",
+  );
+  final wsUri = Uri.parse(url!);
   return ChessConnection(uri: wsUri);
 }
