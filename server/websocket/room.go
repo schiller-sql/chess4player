980f650153e58026e1d4570cf81c773f8b600fa0
Tony <65500763+bit-burger@users.noreply.github.com>
2023-01-07 11:41:12 +0100

    server: adjust room.go to new game system

--- server/websocket/room.go
+++ server/websocket/room.go
@@ -2,7 +2,6 @@ package websocket
 
 import (
 	"log"
-	"server/board"
 	"server/domain"
 	"sync"
 )
@@ -15,7 +14,7 @@ type Room struct {
 	Host             *domain.Client
 	Pool             *Pool
 	InGame           *InGame
-	Game             *board.Board
+	game             *Game
 }
 
 type InGame struct {
@@ -44,152 +43,125 @@ func NewRoom(host *domain.Client, pool *Pool) *Room {
 		Host:             host,
 		Pool:             pool,
 		InGame:           &InGame{value: false, Mutex: sync.Mutex{}},
-		Game:             &board.Board{Players: make(map[*domain.Client]*board.PlayerAttributes)},
 	}
 }
 
-func (this *Room) Start() { //TODO: cant register more than four people
+func (r *Room) Start() { //TODO: cant register more than four people
 	for {
 		select {
-		case participant := <-this.Register:
+		case participant := <-r.Register:
 			log.Println("TRACE register new client in room")
-			this.Participants.Lock()
-			this.Participants.Clients[participant.Client] = participant.Name
-			this.Participants.Unlock()
-			if participant.Client != this.Host {
-				this.participantCountUpdate()
+			r.Participants.Lock()
+			r.Participants.Clients[participant.Client] = participant.Name
+			if participant.Client != r.Host {
+				r.participantCountUpdate()
+			}
+			r.Participants.Unlock()
+			break
+		case client := <-r.UnregisterClient: //only called if client lost connection
+			endsGame := r.leaveRoom(client)
+			if endsGame {
+				return
 			}
 			break
-		case client := <-this.UnregisterClient: //only called if client lost connection
-			log.Println("INFO room lost connection to client")
-			this.Participants.Lock()
-			delete(this.Participants.Clients, client)
-			delete(this.Game.Players, client) //TODO: remove player from game.moveOrder by resizing moveOrder
-			this.Participants.Unlock()
-			if client == this.Host {
-				this.Participants.Lock()
-				this.hostLeft()
-				this.Participants.Unlock()
+		case event := <-r.InputEvent:
+			switch event.Message.Type {
+			case "room":
+				switch event.Message.SubType {
+				case "leave":
+					endsGame := r.leaveRoom(event.Client)
+					if endsGame {
+						return
+					}
+				default:
+					log.Println("WARNING unexpected statement\n   => disconnecting client")
+					event.Client.Disconnect()
+					break
+				}
+				break
+			case "game":
+				switch event.Message.SubType {
+				case "start":
+					log.Println("TRACE room started game")
+					r.InGame.Lock()
+					inGame := r.InGame.value
+					r.InGame.Unlock()
+					if !inGame || event.Client != r.Host {
+						event.Client.Disconnect()
+						break
+					}
+					var content = event.Message.Content
+					r.InGame.value = true
+					time := uint((content["time"]).(float64)) // TODO cast
+					r.Participants.Lock()
+					r.InGame.Lock()
+					r.InGame.value = true
+					r.game = StartGame(r.Participants.Clients, time)
+					r.Participants.Unlock()
+					r.InGame.Unlock()
+					break
+				default:
+					r.InGame.Lock()
+					inGame := r.InGame.value
+					r.InGame.Unlock()
+					if !inGame {
+						break
+					}
+					r.game.Event(event)
+				}
+			default:
+				log.Println("WARNING unexpected statement\n   => disconnecting client")
+				event.Client.Disconnect()
 				break
 			}
-			this.participantCountUpdate()
-			this.InGame.Lock()
-			if this.InGame.value {
-				this.Participants.Lock()
-				this.Game.Player = client
-				this.Game.Resign()
-				this.Participants.Unlock()
-			}
-			this.InGame.Unlock()
-			break
-		case event := <-this.InputEvent:
-			this.handleEvent(event)
-			break
 		}
 	}
 }
 
-func (this *Room) Unregister(client *domain.Client) {
-	this.UnregisterClient <- client
+func (r *Room) Unregister(client *domain.Client) {
+	r.UnregisterClient <- client
 }
 
-func (this *Room) Input(event domain.ClientEvent) {
-	this.InputEvent <- event
+func (r *Room) Input(event domain.ClientEvent) {
+	r.InputEvent <- event
 }
 
-func (this *Room) handleEvent(event domain.ClientEvent) {
-	switch event.Message.Type {
-	case "room":
-		switch event.Message.SubType {
-		case "leave":
-			this.leaveRoom(event)
-			break
-		default:
-			log.Println("WARNING unexpected statement\n   => disconnecting client")
-			event.Client.Disconnect()
-			break
-		}
-		break
-	case "game":
-		switch event.Message.SubType {
-		case "start":
-			log.Println("TRACE room started game")
-			var content = event.Message.Content
-			this.InGame.value = true
-			for participant, name := range this.Participants.Clients {
-				this.Game.Players[participant] = &board.PlayerAttributes{Time: int((content["time"]).(float64)), Name: name}
-			}
-			this.Game.Start()
-			break
-		case "move":
-			var content = event.Message.Content
-			var move [4]int
-			for i, v := range (content["move"]).([]interface{}) {
-				move[i] = int(v.(float64))
-			}
-			this.Game.Move(move, (content["promotion"]).(string))
-			break
-		case "resign":
-			this.Game.Resign()
-			break
-		case "draw-request":
-			this.Game.DrawRequest(event.Client)
-			break
-		case "draw-accept":
-			this.Game.DrawAccept(event.Client)
-			break
-		default:
-			log.Println("WARNING unexpected statement\n   => disconnecting client")
-			event.Client.Disconnect()
-			break
-		}
-	default:
-		log.Println("WARNING unexpected statement\n   => disconnecting client")
-		event.Client.Disconnect()
-		break
-	}
-}
-
-func (this *Room) leaveRoom(event domain.ClientEvent) {
-	log.Println("TRACE unregister client in room")
-	this.Participants.Lock()
-	defer this.Participants.Unlock()
-	this.InGame.Lock()
-	defer this.InGame.Unlock()
-	var client = event.Client
+func (r *Room) leaveRoom(client *domain.Client) (endsGame bool) {
+	r.Participants.Lock()
+	defer r.Participants.Unlock()
+	delete(r.Participants.Clients, client)
 	client.Write("room", "left", map[string]interface{}{})
-	if client == this.Host {
-		this.hostLeft()
-	} else {
-		delete(this.Participants.Clients, client)
-		delete(this.Game.Players, client) //TODO: remove player from game.moveOrder by resizing moveOrder
-		this.Pool.Register <- client
-		client.Handler = this.Pool
-		this.participantCountUpdate()
-		if this.InGame.value {
-			this.Game.Player = client
-			this.Game.Resign()
+	if client == r.Host {
+		r.hostLeft()
+		if r.InGame.value {
+			r.game.ForceGameEnd()
 		}
+		return true
 	}
+	if r.InGame.value {
+		r.game.LeavesRoom(client)
+	}
+	r.participantCountUpdate()
+	return false
 }
 
-func (this *Room) participantCountUpdate() {
-	this.Host.Write(
+func (r *Room) participantCountUpdate() {
+	r.Host.Write(
 		"room",
 		"participants-count-update",
-		map[string]interface{}{"participants-count": len(this.Participants.Clients)})
+		map[string]interface{}{"participants-count": len(r.Participants.Clients)})
 }
 
-func (this *Room) hostLeft() { //TODO: does not work correctly
+func (r *Room) hostLeft() { //TODO: does not work correctly TODO: FIX!!!!!
 	log.Println("DEBUG room disbanded\n   => host left")
-	for client := range this.Participants.Clients {
-		if client != this.Host {
+	for client := range r.Participants.Clients {
+		if client != r.Host {
 			client.Write("room", "disbanded", map[string]interface{}{})
 		}
 	}
-	for client := range this.Participants.Clients {
-		this.Pool.Register <- client
-		client.Handler = this.Pool
+	for client := range r.Participants.Clients {
+		r.Pool.Register <- client
+		client.Handler = r.Pool
 	}
-	this.Pool.UnregisterRoom <- this
+	r.Pool.UnregisterRoom <- r
 }
