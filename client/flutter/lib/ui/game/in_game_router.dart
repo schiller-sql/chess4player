3d467afa91b3b16f37f69b709ddb1028be189369
Tony <65500763+bit-burger@users.noreply.github.com>
2023-03-20 17:42:44 +0100

    client: flutter: remove unnecessary Navigator

--- client/flutter/lib/ui/game/in_game_router.dart
+++ client/flutter/lib/ui/game/in_game_router.dart
@@ -13,37 +13,31 @@ class InGameRouter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Navigator(
-      pages: [
-        MaterialPage(
-          child: BlocListener<GameCubit, GameState>(
-            listener: (context, state) {
-              if (state is GameHasEnded) {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  useRootNavigator: false,
-                  builder: (_) => MultiBlocProvider(
-                    providers: [
-                      BlocProvider.value(
-                        value: context.read<InRoomCubit>(),
-                      ),
-                      BlocProvider.value(
-                        value: context.read<JoinGameCubit>(),
-                      ),
-                      BlocProvider.value(
-                        value: context.read<RoomCubit>(),
-                      ),
-                    ],
-                    child: GameEndedDialog(gameEndReason: state.gameEndReason),
-                  ),
-                );
-              }
-            },
-            child: const GamePage(),
-          ),
-        ),
-      ],
+    return BlocListener<GameCubit, GameState>(
+      listener: (context, state) {
+        if (state is GameHasEnded) {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            useRootNavigator: false,
+            builder: (_) => MultiBlocProvider(
+              providers: [
+                BlocProvider.value(
+                  value: context.read<InRoomCubit>(),
+                ),
+                BlocProvider.value(
+                  value: context.read<JoinGameCubit>(),
+                ),
+                BlocProvider.value(
+                  value: context.read<RoomCubit>(),
+                ),
+              ],
+              child: GameEndedDialog(gameEndReason: state.gameEndReason),
+            ),
+          );
+        }
+      },
+      child: const GamePage(),
     );
   }
 }
