eba41ab1f2d9b51e6cd7c752d46bbc972c770f73
Tony <65500763+bit-burger@users.noreply.github.com>
2023-04-09 16:01:50 +0200

    client: flutter: revamp pubspec.yaml
    - update dependencies
    - format and add comments

--- client/flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import bitsdojo_window_macos
-import shared_preferences_macos
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
