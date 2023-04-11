c1de3e0e2655ded59c6d8551be79aaaf01be41bf
Tony <65500763+bit-burger@users.noreply.github.com>
2023-04-11 08:41:03 +0200

    client: flutter: update theme

--- client/flutter/lib/theme/chess_theme.dart
+++ client/flutter/lib/theme/chess_theme.dart
@@ -2,28 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chess_4p/flutter_4p_chess.dart';
 import 'package:flutter_nord_theme/flutter_nord_theme.dart';
 
-final baseColors = DirectionalTuple(
-  NordColors.$9,
-  NordColors.$13,
-  NordColors.$14,
-  NordColors.$15,
-  Color.lerp(NordColors.$2, NordColors.$0, 0.6)!
-);
+const baseColors = DirectionalTuple(
+      NordColors.$9,
+      NordColors.$13,
+      NordColors.$14,
+      NordColors.$15,
+      Colors.transparent,
+    );
 
-final accentColors = baseColors.map(
-  (color) => Color.lerp(Colors.black, color, 0.65)!,
-).copyWith(inactive: Color.lerp(Colors.black, NordColors.$1, 0.75)!);
+final accentColors = baseColors
+    .map(
+      (color) => Color.lerp(Colors.black, color, 0.6)!,
+    )
+    .copyWith(
+      inactive: Color.lerp(NordColors.$2, NordColors.snowStorm.darkest, 0.125)!,
+    );
 
 final playerStyles = WikiPiecesPlayerStyles(
-  accentColors: accentColors,
-  baseColors: baseColors,
-);
+      accentColors: accentColors,
+      baseColors: baseColors,
+    );
+
+final inactiveBaseTextColor = Color.lerp(NordColors.$3, Colors.white, 0.08)!;
 
 final chessStyle = ChessBoardColorStyle(
-  backgroundColor: Colors.transparent,
-  fieldColor1: NordColors.$1,
-  fieldColor2: NordColors.$0,
-  selectedFieldColor: NordColors.$3,
-  inactiveAccentTextColor: NordColors.$0,
-  inactiveBaseTextColor: NordColors.$1.withAlpha(200),
-);
+      backgroundColor: Colors.transparent,
+      fieldColor1: NordColors.$1,
+      fieldColor2: NordColors.$0,
+      selectedFieldColor: NordColors.$3,
+      inactiveAccentTextColor: Color.lerp(
+        Colors.black,
+        inactiveBaseTextColor,
+        0.65,
+      ),
+      inactiveBaseTextColor: inactiveBaseTextColor,
+    );
